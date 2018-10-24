c5019782d34b8c637e9bb6cfbf26fc45fd65b393
Kairat <kairo@list.ru>
2018-10-24 12:30:43 +0600

    first commit

--- CarouselViewSlider/MainActivity.cs
+++ CarouselViewSlider/MainActivity.cs
@@ -3,17 +3,35 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Com.Gigamole.Infinitecycleviewpager;
+using System.Collections.Generic;
 
 namespace CarouselViewSlider
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]  //AppTheme
     public class MainActivity : AppCompatActivity
     {
+        List<int> listImage = new List<int>();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            InitData();
+            HorizontalInfiniteCycleViewPager viewPager = FindViewById<HorizontalInfiniteCycleViewPager>(Resource.Id.horizontal_viewpager);
+            MyAdapter adapter = new MyAdapter(listImage, BaseContext);
+            viewPager.Adapter = adapter;
+
+        }
+
+        private void InitData()
+        {
+            listImage.Add(Resource.Drawable.intro1);
+            listImage.Add(Resource.Drawable.intro2);
+            listImage.Add(Resource.Drawable.intro3);
+
+
         }
     }
 }
---
+++ CarouselViewSlider/MyAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Java.Lang;
+using Android.Support.V4.View;
+using Android.Views;
+using Android.Widget;
+
+namespace CarouselViewSlider
+{
+    public class MyAdapter: PagerAdapter
+    {
+        List<int> ListImages;
+        Context context;
+        LayoutInflater LayoutInflater;
+
+        public MyAdapter(List<int> ListImages, Context context)
+        {
+            this.ListImages = ListImages;
+            this.context = context;
+            LayoutInflater = LayoutInflater.From(context);
+        }
+
+        public override Java.Lang.Object InstantiateItem(ViewGroup container, int position)
+        {
+            View view = LayoutInflater.Inflate(Resource.Layout.Card_Item, container, false);
+            ImageView imageView = view.FindViewById<ImageView>(Resource.Id.imageview);
+            imageView.SetImageResource(ListImages[position]);
+            container.AddView(view);
+            return view;
+
+        }
+
+        public override int Count => ListImages.Count;
+
+        public override bool IsViewFromObject(View view, Java.Lang.Object objectValue)
+        {
+            return view.Equals(objectValue);
+        }
+        public override void DestroyItem(ViewGroup container, int position, Java.Lang.Object objectValue)
+        {
+            container.RemoveView((View)objectValue);
+        }
+    }
+}
